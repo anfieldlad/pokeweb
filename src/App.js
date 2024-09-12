84b39ab40e8b5a2207480d36c8725eb775f9b70c
Bobby Ananta Dioriza <bobby.dioriza@adira.co.id>
2024-09-12 17:55:48 +0700

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { AppBar, Toolbar, Typography, Container, Button } from '@mui/material';
+import { AppBar, Toolbar, Typography, Container, Button, Box } from '@mui/material';
 import HomePage from './pages/HomePage';
 import DetailPage from './pages/DetailPage';
 import FavoritesPage from './pages/FavoritesPage';
@@ -22,13 +22,20 @@ function App() {
       </AppBar>
 
       {/* Main container untuk konten halaman */}
-      <Container sx={{ marginTop: '20px' }}>
+      <Container sx={{ marginTop: '20px', minHeight: '80vh' }}>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/pokemon/:pokemonName" element={<DetailPage />} />
           <Route path="/favorites" element={<FavoritesPage />} />
         </Routes>
       </Container>
+
+      {/* Footer */}
+      <Box component="footer" sx={{ bgcolor: 'primary.main', p: 2, mt: 4, textAlign: 'center', color: 'white' }}>
+        <Typography variant="body2">
+          by: Bobby Ananta Dioriza feat. ChatGPT
+        </Typography>
+      </Box>
     </Router>
   );
 }
